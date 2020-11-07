beb87cebff2551db27c5ae5edb6844f6f495abf9
Mickaël Pham <inbox@mickael.dev>
2020-11-06 17:12:12 -0800

    feat: idea plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,11 @@ plugins {
     id("com.diffplug.spotless")
 }
 
+
+repositories {
+    jcenter()
+}
+
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
 plugins {
   id("com.diffplug.spotless") version "5.7.0" apply false
 }
-
-repositories {
-    jcenter()
-}
